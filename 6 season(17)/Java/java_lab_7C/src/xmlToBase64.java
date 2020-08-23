dd6008d08f7a4c7a5eefd9a92591b0c326f91a16
ikhee <iheeizee@gmail.com>
2020-08-23 23:54:59 +0800

    dadlagiin tailan zoriulagdsan file belen

--- 6 season(17)/Java/java_lab_7C/src/xmlToBase64.java
+++ 6 season(17)/Java/java_lab_7C/src/xmlToBase64.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
+import java.io.*;
+import java.util.Base64;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class xmlToBase64 {
+    public static void main(String[] args) {
+        String originalInput = "<root> dsfd </root>";
+        String encodedString = Base64.getEncoder().encodeToString(originalInput.getBytes());
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+        String decodedString = new String(decodedBytes);
+        System.out.println(hexStringToByteArray(encodedString));
+        
+        try {
+            byte [] hell = compressString("werddsafaosjf ljl;dsfj d;lasjf");
+            System.out.println(hell);
+            System.out.println(uncompressString(hell));
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
+    public static byte[] compressString(String srcTxt)
+            throws IOException {
+        ByteArrayOutputStream rstBao = new ByteArrayOutputStream();
+        GZIPOutputStream zos = new GZIPOutputStream(rstBao);
+        zos.write(srcTxt.getBytes("UTF-8"));
+        byte[] bytes = rstBao.toByteArray();
+        zos.close();
+        return bytes;
+    }
+
+    public static String uncompressString(byte [] zippedBase64Str)
+            throws IOException {
+        String result = "";
+        GZIPInputStream zi = null;
+        String readed;
+        zi = new GZIPInputStream(new ByteArrayInputStream(zippedBase64Str));
+        BufferedReader bf = new BufferedReader(new InputStreamReader(zi, "UTF-8"));
+        while ((readed = bf.readLine()) != null) {
+            result += readed;
+        }
+        return result;
+    }
 }
--- 6 season(17)/Java/java_lab_7C/src/xmlToZip.java
+++ 6 season(17)/Java/java_lab_7C/src/xmlToZip.java
@@ -1,4 +1,50 @@
-package PACKAGE_NAME;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
 public class xmlToZip {
+    public static void main(String[] args) throws Exception {
+        String string = "test" +
+                "dsfdsfsf\r\n" +
+                "dsfdsfdsf";
+        System.out.println("after compress:");
+        byte[] compressed = compress(string);
+        System.out.println(compressed);
+        System.out.println("after decompress:");
+        String decomp = decompress(compressed);
+        System.out.println(decomp);
+    }
+
+    public static byte[] compress(String str) throws Exception {
+        if (str == null || str.length() == 0) {
+            return null;
+        }
+        System.out.println("String length : " + str.length());
+        ByteArrayOutputStream obj=new ByteArrayOutputStream();
+        GZIPOutputStream gzip = new GZIPOutputStream(obj);
+        gzip.write(str.getBytes(StandardCharsets.UTF_8));
+        gzip.close();
+
+        return obj.toByteArray();
+    }
+
+    public static String decompress(byte[] str) throws Exception {
+        if (str == null ) {
+            return null;
+        }
+
+        GZIPInputStream gis = new GZIPInputStream(new ByteArrayInputStream(str));
+        BufferedReader bf = new BufferedReader(new InputStreamReader(gis, StandardCharsets.UTF_8));
+        StringBuilder outStr = new StringBuilder();
+        String line;
+        while ((line=bf.readLine())!=null) {
+            outStr.append(line);
+        }
+        System.out.println("Output String lenght : " + outStr.length());
+        return outStr.toString();
+    }
 }
